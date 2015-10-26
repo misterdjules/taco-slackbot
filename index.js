b44e4d33db36a3092f3a96e81863e768a657f871
Julien Gilli <julien.gilli@joyent.com>
2015-10-26 14:53:06 -0700

    Initial revision

---
+++ index.js
@@ -0,0 +1,49 @@
+const assert = require('assert-plus');
+const http = require('http');
+
+const Slack = require('slack-client');
+
+const SLACK_TOKEN = process.env.SLACK_TOKEN;
+assert.string(SLACK_TOKEN, 'The SLACK_TOKEN environment variable must be set');
+
+const TACODAY_API_ADDR = process.env.TACODAY_API_PORT_8080_TCP_ADDR;
+assert.string(TACODAY_API_ADDR,
+    'The TACODAY_API_PORT_8080_TCP_ADDR environment variable must be set');
+
+const autoReconnect = true;
+const autoMark = true;
+
+const slack = new Slack(SLACK_TOKEN, autoReconnect, autoMark);
+
+slack.on('open', function onOpen() {
+    console.log(`Connected to ${slack.team.name} as ${slack.self.name}`);
+});
+
+slack.on('message', function (message) {
+    var channel;
+    var wotdReq;
+
+    if (message.text && message.text.includes('wotd')) {
+        channel = slack.getChannelGroupOrDMByID(message.channel);
+        wotdReq = http.get(`http://${TACODAY_API_ADDR}:8080/wotd`, function onWotdRes(res) {
+            var wotd = '';
+            res.on('data', function onData(data) {
+                wotd += data.toString();
+            });
+
+            res.on('end', function onEnd() {
+                channel.send('Si senor! The word of the day is ' + wotd + '!');
+            });
+        });
+
+        wotdReq.on('error', function onError(err) {
+            channel.send('Sorry senor, I couldn\'t get the word of the day this time!');
+        });
+    }
+});
+
+slack.on('error', function (err) {
+    console.error("Error", err);
+})
+
+slack.login();
